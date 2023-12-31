e56bca70276534816e417b5d18da96823e68270e
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-12-31 13:50:42 +0000

    Arrays

---
+++ matrix.py
@@ -0,0 +1,6 @@
+mport numpy as np 
+matrix_a = np.array([[1, 2], [3, 4]])
+matrix_b = np.array([[5, 6], [7, 8]])
+
+matrix_product = np.dot(matrix_a, matrix_b)  # Matrix multiplication
+print(matrix_product)
